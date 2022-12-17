76342fb0314f2ec7a94b6adc515514b6aa6a9626
molodec1337322 <sshibanov1@yandex.ru>
2022-12-18 02:21:57 +0300

    files

--- src/main/java/com.example.kursach3/controllers/TicketController.java
+++ src/main/java/com.example.kursach3/controllers/TicketController.java
@@ -405,6 +405,9 @@ public class TicketController {
             return "redirect:/access_denial";
         }
 
+        List<FileModel> filesList = fileModelDAO.getAllFilesByAnswers(id);
+
+        model.addAttribute("files", filesList);
         model.addAttribute("ticketTopic", answerDAO.GetAnswerByID(id).getTicket().getTopic());
         model.addAttribute("madeBy", answerDAO.GetAnswerByID(id).getUser().getFirst_name() + " " +
                 answerDAO.GetAnswerByID(id).getUser().getLast_name() + " " +
@@ -565,6 +568,9 @@ public class TicketController {
             return "redirect:/access_denial";
         }
 
+        List<FileModel> filesList = fileModelDAO.getAllFilesByTicket(ticketDAO.getTicketByID(id).getId());
+
+        model.addAttribute("files", filesList);
         model.addAttribute("logged_user", username);
         model.addAttribute("answer", answerDAO.GetAnswerByID(id));
         model.addAttribute("ticket", ticketDAO.getTicketByID(answerDAO.GetAnswerByID(id).getTicket().getId()));
